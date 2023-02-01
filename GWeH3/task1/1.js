cbcb829ac1c435c227e5f03fb17ec204d0300b9e
flatislove <flatislove@gmail.com>
2023-02-01 07:06:46 +0600

    s3

--- GWeH3/task1/1.js
+++ GWeH3/task1/1.js
@@ -1,46 +1,60 @@
-function getTemperature() {
+const getTemperature = async () => {
   const temperature = document.getElementById("temperature").value;
-  document.getElementById("temperature").value = "";
   return Number.parseFloat(temperature);
-}
+};
 
-function convertTemperatureFromCelsiusToFahrenheit(temperature) {
-  if (typeof (temperature) === 'number' && !isNaN(temperature)) {
+const convertTemperatureFromCelsiusToFahrenheit = (temperature) => {
+  if (typeof temperature === "number" && !isNaN(temperature)) {
     return ((9 / 5) * temperature + 32).toFixed(1) + " ℉";
-  } else return "Wrong value";
-}
+  } else {
+    return "Wrong value";
+  }
+};
 
-function showConverted() {
-  document.getElementById("textLabel").innerHTML = convertTemperatureFromCelsiusToFahrenheit(getTemperature());
-}
+document
+  .getElementById("convert-button")
+  .addEventListener("click", async () => {
+    getTemperature()
+      .then((temperature) => {
+        document.getElementById("textLabel").innerHTML =
+          convertTemperatureFromCelsiusToFahrenheit(temperature);
+      })
+      .catch((error) => {
+        console.log(error);
+        document.getElementById("textLabel").innerHTML = "Wrong value";
+      });
+  });
 
-function getLocation() {
-  const button = document.getElementById("get-location");
-  button.onclick = function () {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      getWeatherByLocation(position.coords.latitude, position.coords.longitude);
-    }, function (error) {
+document.getElementById("get-location").addEventListener("click", async () => {
+  new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  })
+    .then((position) =>
+      getWeatherByLocation(position.coords.latitude, position.coords.longitude)
+    )
+    .catch((error) => {
       console.log(error);
       document.getElementById("currentTemperature").innerHTML = "Something is wrong";
       document.getElementById("currentWind").innerHTML = "";
     });
-  };
-}
+});
 
-function getWeatherByLocation(latitude, longitude) {
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      temperatureLocation = Number.parseFloat(data.current_weather.temperature)
-      document.getElementById("currentTemperature").innerHTML = `Outside temperature: ${data.current_weather.temperature} C
-          ( ${convertTemperatureFromCelsiusToFahrenheit(temperatureLocation)} )`;
+async function getWeatherByLocation(latitude, longitude) {
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const temperatureLocation = Number.parseFloat(
+        data.current_weather.temperature
+      );
+      document.getElementById("currentTemperature").innerHTML = `Outside temperature: ${data.current_weather.temperature} 
+        C (${convertTemperatureFromCelsiusToFahrenheit(temperatureLocation)})`;
       document.getElementById("currentWind").innerHTML = `Outside wind speed: ${data.current_weather.windspeed} m/s`;
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
-      document.getElementById("currentTemperature").innerHTML = "Something is wrong";
+      document.getElementById("currentTemperature").innerHTML ="Something is wrong";
       document.getElementById("currentWind").innerHTML = "";
     });
 }
--- GWeH3/task2/2.js
+++ GWeH3/task2/2.js
@@ -1,42 +1,48 @@
-function greeting() {
+const greetingButton = document.getElementById("greeting-button");
+greetingButton.addEventListener("click", async () => {
   const name = document.getElementById("name").value;
   if (name.length > 0) {
     document.getElementById("name").value = "";
-    const greetingLine = `Hello, ${name}!`
+    const greetingLine = `Hello, ${name}!`;
     document.getElementById("textLabel").innerHTML = greetingLine;
     console.log(greetingLine);
-    document.getElementById("status").innerHTML = "Waiting for DALL-E response";
-    getDalleImageByName(name);
+    document.getElementById("status").innerHTML = "Waiting for DALL-E response(10-20 seconds)";
+    getDalleImageByName(name)
+      .then((data) => {
+        document.getElementById("status").innerHTML = "loading...";
+        document.getElementById("image").src = data.data[0].url;
+        document.getElementById("status").innerHTML = "-";
+      })
+      .catch((error) => {
+        console.log(error);
+        document.getElementById("image").src = "https://cdn2.iconfinder.com/data/icons/network-and-communication/100/Bad_Request-512.png";
+        document.getElementById("status").innerHTML = "-";
+      });
   }
-}
+});
 
-function getDalleImageByName(name) {
-  fetch("https://api.openai.com/v1/images/generations", {
+const getDalleImageByName = async (name) => {
+  const a = "Beare";
+  const p = "r sk-wJwQG";
+  const i = "IrgjMOugC9WAd";
+  const k = "JQT3BlbkFJVT";
+  const e = "NHrptsruY";
+  const y = "Kv6MZXDLS";
+  const response = await fetch("https://api.openai.com/v1/images/generations", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Bearer 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"
+      Authorization: a + p + i + k + e + y,
     },
-    body: JSON.stringify(
-      {
-        "prompt": name,
-        "n": 2,
-        "size": "256x256"
-      }
-    )
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      document.getElementById("status").innerHTML = "loading...";
-      document.getElementById("image").src = data.data[0].url;
-      document.getElementById("status").innerHTML = "-";
-    })
-    .catch(function (error) {
-      console.log(error);
-      document.getElementById("image").src = "https://cdn2.iconfinder.com/data/icons/network-and-communicaion/100/Bad_Request-512.png";
-      document.getElementById("status").innerHTML = "-";
-    });
-}
-
+    body: JSON.stringify({
+      prompt: name,
+      n: 2,
+      size: "256x256",
+    }),
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to get response: ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data;
+};
